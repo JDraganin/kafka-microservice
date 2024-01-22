189a15a31f50657670a8c05a86e7b7ec5394656e
JDraganin <92109961+JDraganin@users.noreply.github.com>
2024-01-22 10:22:13 +0200

    initial commit

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/kafka/service/TwitterToKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.twitter.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaApplicationTests {
+    @Test
+    public void contextLoad()
+    {
+
+    }
+}
